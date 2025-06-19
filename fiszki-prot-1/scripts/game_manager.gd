68476102a7fdbc7aaf07fdebef3c95a31a16c473
KonradKamilChwalowski <ace55@wp.pl>
2025-06-19 23:13:50 +0200

    Add main menu with yellow background and working buttons

---
+++ fiszki-prot-1/scripts/game_manager.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var menu_screen := load("res://scenes/screens/menu_screen.tscn")
+var language_choice_screen := load("res://scenes/screens/language_choice_screen.tscn")
+var stats_screen := load("res://scenes/screens/stats_screen.tscn")
+var options_screen := load("res://scenes/screens/options_screen.tscn")
+var actual_screen: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#get_tree().change_scene_to_packed(menu_scene)
+	actual_screen = menu_screen.instantiate()
+	add_child(actual_screen)
+
+func change_screen(screen_name: String):
+	
+	if screen_name == "menu_screen":
+		actual_screen.queue_free()
+		actual_screen = menu_screen.instantiate()
+		add_child(actual_screen)
+		
+	if screen_name == "language_choice_screen":
+		actual_screen.queue_free()
+		actual_screen = language_choice_screen.instantiate()
+		add_child(actual_screen)
+		
+	if screen_name == "stats_screen":
+		actual_screen.queue_free()
+		actual_screen = stats_screen.instantiate()
+		add_child(actual_screen)
+		
+	if screen_name == "options_screen":
+		actual_screen.queue_free()
+		actual_screen = options_screen.instantiate()
+		add_child(actual_screen)
